2fbc46432366cb75b38a2420c2c6a671516320f0
fetilla <fetilla@gmail.com>
2019-11-13 02:50:25 +0100

    Detected error in header popular button, fixed.

--- frontend/src/components/header/Headers.tsx
+++ frontend/src/components/header/Headers.tsx
@@ -4,9 +4,13 @@ import history from '../../navigation/history';
 import { observer } from 'mobx-react';
 import { Navbar, Nav, Col } from 'react-bootstrap';
 import AuthStorage from '../auth/Auth.storage';
+import ThingStorage from '../thing/ThingStorage';
 
-const navigatePopular = () => {
-  history.push('/popular');
+const NavigatePopular = () => {
+  const popularRef = '/popular';
+  if(window.location.pathname !== popularRef) {
+    history.push(popularRef);
+  }
 }
 
 @observer
@@ -15,7 +19,7 @@ export class Header extends React.Component {
     return (
       <Navbar bg="dark" expand="lg">
       {(AuthStorage.bearer_token) ?
-        (<Nav.Link href="/popular">Popular</Nav.Link>)
+        (<Nav.Link onClick={NavigatePopular}>Popular</Nav.Link>)
         : (null)
       }
       <Col md={{ span: 4, offset: 4 }}><Navbar.Text>
